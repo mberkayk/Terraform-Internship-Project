2f698dd65fe9ff265f9bc38b303ff17ece25f791
mberkayk <berkaykuman@live.com>
2020-11-23 08:33:32 +0300

    added vault_aws_backend, vault_backend_role resources

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+variable "name" {default="dynamic-vault-admin"}
+
+provider "vault" {}
+
 provider "aws" {
 	region = "us-east-1"
 	access_key = var.root-access-key
@@ -16,6 +20,31 @@ provider "aws" {
 	region = "us-east-1"
 	access_key = var.s3-access-key
 	secret_key = var.s3-secret-key
+}
+
+resource "vault_aws_secret_backend" "aws"{
+	access_key = var.root-access-key
+	secret_key = var.root-secret-key
+	name = ""${var.name}-path"
+
+  default_lease_ttl_seconds = "120"
+  max_lease_ttl_seconds     = "240"
+}
+
+resource "vault_aws_secret_backend_role" "ec2-role" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "dynamic-vault-ec2-role"
+  credential_type = "iam_user"
+
+  policy_arns = ["arn:aws:iam::427790202393:policy/ec2-Policy"]
+}
+
+resource "vault_aws_secret_backend_role" "s3-role" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "dynamic-vault-s3-role"
+  credential_type = "iam_user"
+
+  policy_arns = ["arn:aws:iam::427790202393:policy/S3-Policy"]
 }
 
 terraform {
