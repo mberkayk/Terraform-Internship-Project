88d3580b9cb09fcbea919afa54f8e6cbe6fd1ea8
mberkayk <berkaykuman@live.com>
2020-11-23 12:49:59 +0300

    update

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ resource "vault_aws_secret_backend_role" "s3-role" {
   policy_arns = ["arn:aws:iam::427790202393:policy/S3-Policy"]
 }
 
+
 data "vault_aws_access_credentials" "ec2-creds" {
   backend = vault_aws_secret_backend.aws.path
   role    = vault_aws_secret_backend_role.ec2-role.name
@@ -35,6 +36,7 @@ data "vault_aws_access_credentials" "s3-creds" {
   backend = vault_aws_secret_backend.aws.path
   role    = vault_aws_secret_backend_role.s3-role.name
 }
+
 
 provider "aws" {
 	region = "us-east-1"
@@ -65,7 +67,7 @@ terraform {
   }
 }
 
-/*
+
 resource "aws_instance" "cloud_server" {
 	provider = aws.ec2
 	ami = "ami-0817d428a6fb68645"
@@ -73,7 +75,7 @@ resource "aws_instance" "cloud_server" {
 	tags = {
 		Name = "Terraform Cloud Instance"
 	}
-}*/
+}
 
 
 resource "aws_s3_bucket" "mybucket" {
